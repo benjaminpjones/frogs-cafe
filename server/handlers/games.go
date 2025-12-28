2d63b7809feaf86709b3ab874cb3a16ea3a5c02a
Benjamin P. Jones <benjaminpaikjones@gmail.com>
2025-12-27 21:08:27 -0500

    Move SaveMove handler to games.go

--- server/handlers/games.go
+++ server/handlers/games.go
@@ -13,6 +13,33 @@ import (
 	"github.com/go-chi/chi/v5"
 )
 
+func (h *Handler) SaveMove(gameIDStr string, playerID int, data map[string]interface{}) error {
+	gameID, err := strconv.Atoi(gameIDStr)
+	if err != nil {
+		return err
+	}
+
+	x := int(data["x"].(float64))
+	y := int(data["y"].(float64))
+
+	// Get the current move number
+	var moveNumber int
+	err = h.db.QueryRow(
+		"SELECT COALESCE(MAX(move_number), 0) + 1 FROM moves WHERE game_id = $1",
+		gameID,
+	).Scan(&moveNumber)
+	if err != nil {
+		return err
+	}
+
+	// Insert the move
+	_, err = h.db.Exec(
+		"INSERT INTO moves (game_id, player_id, move_number, x, y) VALUES ($1, $2, $3, $4, $5)",
+		gameID, playerID, moveNumber, x, y,
+	)
+	return err
+}
+
 func (h *Handler) ListGames(w http.ResponseWriter, r *http.Request) {
 	// Support filtering by status query parameter
 	status := r.URL.Query().Get("status")
